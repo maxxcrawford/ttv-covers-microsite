d7bcefb5168fae3d0167300126f0fa8faf533685
Maxx Crawford <mcrawford@broco.com>
2018-02-06 20:51:22 -0600

    Added analytics, updated JS to not be inline

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+		$(".js-lightGallery").lightGallery({
+			download: false,
+			autoplay: false,
+			autoplayControls: false,
+			actualSize: false,
+			fullScreen: false,
+			showThumbByDefault: false,
+			counter: false,
+			selector: '.c-cover-grid__item'
+		});
+		// var myLazyLoad = new LazyLoad();
+});
